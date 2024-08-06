852f9e0f7d14938d9bb4b1f3f0ed6558b7a415e2
CHIRUMAMILLA GAMANA <153715131+gamana29@users.noreply.github.com>
2024-08-06 19:36:06 +0530

    Add files via upload

---
+++ Bubblesort.asm
@@ -0,0 +1,18 @@
+MOV SI,500H
+    MOV CL,[SI]
+    DEC CL
+   REPEAT: MOV SI,500H
+    MOV CH,[SI]  
+    DEC CH
+    INC SI        
+   COMP: MOV AL,[SI]
+    INC SI
+    CMP AL,[SI]
+    JC AHEAD
+    XCHG AL,[SI]
+    XCHG AL,[SI-1]
+    AHEAD:DEC CH
+    JNZ COMP
+    DEC CL
+    JNZ REPEAT
+    HLT
